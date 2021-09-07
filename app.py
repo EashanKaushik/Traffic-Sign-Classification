3012f7c97a1968132546c6ce962694d952a0b774
EashanKaushik <Eashank16@gmail.com>
2021-09-07 09:54:14 -0400

    website inital content

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 app.add_url_rule('/home', 'home', views.home, methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
